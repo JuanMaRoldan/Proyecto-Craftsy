0ae4f3b5c1493ed7c1e08c07495b2b0f12e4a5bc
JuanMaRoldan <juanmanuel.roldan@outlook.com>
2022-04-04 16:56:15 -0300

    agrego estructura header

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.get('/',(req,res) => res.sendFile(path.resolve(__dirname,'views','index.html
 app.get('/login',(req,res) => res.sendFile(path.resolve(__dirname,'views','login.html')))
 app.get('/productCart',(req,res) => res.sendFile(path.resolve(__dirname,'views','productCart.html')))
 app.get('/productDetail',(req,res) => res.sendFile(path.resolve(__dirname,'views','productDetail.html')))
-app.get('/registe',(req,res) => res.sendFile(path.resolve(__dirname,'views','register.html')))
+app.get('/register',(req,res) => res.sendFile(path.resolve(__dirname,'views','register.html')))
 
 
 // catch 404 and forward to error handler
